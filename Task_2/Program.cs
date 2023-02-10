123993d9c43c994709582e148d3c74dfaa434fa8
Alex <abreskolenko@yandex.ru>
2023-02-11 00:21:17 +0300

    Create task 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string str;
+int num_1, num_2;
+
+Console.Write("Enter the first number : ");
+str = Console.ReadLine();
+
+num_1 = Convert.ToInt32(str);
+
+Console.Write("Enter the second number : ");
+str = Console.ReadLine();
+
+num_2 = Convert.ToInt32(str);
+
+if (num_1 > num_2)
+{
+    Console.WriteLine("Max number = " + num_1);
+}
+else
+{
+    Console.WriteLine("Max number = " + num_2);
+}

3214d036492ccf1a70f4e185f7fdc00a14028d20
Alex <abreskolenko@yandex.ru>
2023-02-11 00:59:34 +0300

    Add task 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+string str;
+int number;
+
+Console.Write("Enter the number : ");
+str = Console.ReadLine();
+number = Convert.ToInt32(str);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
